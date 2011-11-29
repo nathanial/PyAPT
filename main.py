95cca6af476354cdaea59865d10dda363175b910
Nathan Hartman <nathanial.hartman@gmail.com>
2011-11-29 13:49:10 -0600

    so far unable to find right crc16

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
-import serial
-port = serial.Serial(raw_input("Port:"))
+import serial, struct, crc16pure
+port = serial.Serial("COM79")
 buffer = []
+print ""
 
     # h = "%02X" % buffer[-1]
     # print h,
 
-def unescape(buffer):
+def escape(buffer):
     return buffer
     i = 0
-    new_buffer = []
+    nbuf = []
     while i < len(buffer) - 2:
         if buffer[i] == 0x04 and buffer[i+1] == 0x04:
-            new_buffer.append(0x04)
+            nbuf.append(0x04)
             i += 2
         else:
-            new_buffer.append(buffer[i])
+            nbuf.append(buffer[i])
             i += 1
-    new_buffer.append(buffer[-2])
-    new_buffer.append(buffer[-1])
-    return new_buffer
+    nbuf.append(buffer[-2])
+    nbuf.append(buffer[-1])
+    return nbuf
 
 def print_data(buffer):
-    buffer = unescape(buffer)
     print "DDD: %s" % (" ".join(["%02X" % x for x in buffer[1:-2]]),)
+
+def crc_is_correct(packet):
+    data = packet[1:-4] #trim off packet wrapper
+    print "DAT: %s" % " ".join(["%02X" % x for x in data])
+    s = crc16pure.crc16([chr(x) for x in data])
+    print s
+    crcBytes = packet[-4:-2]
+    crcBytes.reverse()
+    print " ".join(["%02X" % x for x in crcBytes])
+    crcBytes = "".join([chr(x) for x in crcBytes])
+    crc = struct.unpack('H', crcBytes)
+    print crc
+    print "checksum: expected %s, got %s" % (s, crc)
+    return s == bytes
 
 def print_from_buffer(): 
     global buffer
-    if buffer[-1] == 0x04 and buffer[-2] == 0x06: 
+    escaped = escape(buffer)
+    if len(escaped) <= 3: return
+    if escaped[-1] == 0x04 and escaped[-2] == 0x06: 
         print "RAW: %s" % " ".join(["%02X" % x for x in buffer])
-        if buffer[0] == 0x06:
-            print_data(buffer)
+        if escaped[0] == 0x06:
+            if crc_is_correct(escaped):
+                print_data(escaped)
+            else:
+                print "ERR: Bad Checksum"
         else:
-            print ""
+            print "ERR: Malformed Packet"
         buffer = []
         print ""
 
